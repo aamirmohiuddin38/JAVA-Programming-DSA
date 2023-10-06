7bddf85c4b027e98b1ffd52f676951119e906c0d
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2023-10-06 17:54:54 +0530

    Hello World Program

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World! Welcome to  Java Universe");
+        System.out.println("Java is Platform independent.");
+    }
+}
