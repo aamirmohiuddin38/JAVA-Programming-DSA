104429f8b7f5431eb0984fbfb2f66f5174e0f31f
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-03 21:22:22 +0530

    Formatted print and clear console command

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main{
     public static void main(String[] args){
+        System.out.print("\033\143"); //for clearing console
+
         System.out.println("Hello World! Welcome to  Java Universe");
         System.out.println("Java is Platform independent.");
+        System.out.println("--------------------------------------------\n\n");
     }
 }
