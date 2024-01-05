40abf22e688eb2ea7bb6e388d84ca9c946b03445
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-05 19:20:17 +0530

    RestaurantBill - exploring System.out.println()

---
+++ RestaurantBill.java
@@ -0,0 +1,33 @@
+public class RestaurantBill {
+    public static void main(String args[]){
+        System.out.print("\033\143"); //for clearing console
+
+        System.out.println("\t\t\t\tZEWA(KFC)");
+        System.out.println("\t\t\t    KASHMIR FOOD CENTER");
+        System.out.println("-----------------------------------------------------------------------------");
+
+        System.out.println("________________________________________________________________________");
+        System.out.println("| Bill To: \t\t\t\t\t Bill No: A001390\t|");
+        System.out.println("| AYATHMULLA \t\t\t\t\t Date: 03-01-202\t|");
+        System.out.println("| 6005899633 \t\t\t TimeStamp: 03-01-2024 05:45:32 PM\t|");
+        System.out.println("|_______________________________________________________________________|");
+        System.out.println("_______________________________________________________");
+        System.out.println("| S.No | \t Description \t | Qty | Rate |\tAmount |");
+        System.out.println("|______|_________________________|_____|______|________|");
+        System.out.println("| 1    | BIRYANI CHICKEN HALF \t |  1.0|  130 |     130|");
+        System.out.println("|      |                         |     |      |        |");
+        System.out.println("| 2    | DC30                 \t |  1.0|   30 |      30|");
+        System.out.println("|______|_________________________|_____|______|________|");
+        System.out.println("|      |                              SubTotal|     160|");
+        System.out.println("|______|______________________________________|________|");
+    
+        System.out.println("BANK DETAILS:");
+        System.out.println("ACC NO: *********0012");
+        System.out.println("Mobile No: 0002223334");
+        System.out.println("IFSC: JAKA0KASH");
+
+        System.out.println("\t\t\t\t Thanks Visit Again ");
+        System.out.println("-------------------------------------------------------------------------------");
+
+    }
+}
