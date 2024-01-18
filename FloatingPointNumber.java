7045e27b18f2bd2cf9cd9c19c46fb81fabe2bc5f
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-18 23:59:56 +0530

    Floating point numbers and diplaying

---
+++ FloatingPointNumber.java
@@ -0,0 +1,25 @@
+// Floating Point Numbers - Beginner
+import java.util.Scanner;
+public class FloatingPointNumber {
+    public static void main(String args[]){
+        System.out.println("\033\143");
+        System.out.println("Java | Programming");
+        System.out.println("-------------------------------------");
+        System.out.println(" Floating Point Numbers \n");
+
+        Scanner input = new Scanner(System.in);
+        float fnum;
+        System.out.print("Enter fnum: ");
+        fnum = input.nextFloat();
+
+        // Displaying in various formats
+        System.out.println("\n\tResults...");
+        System.out.println("\t-----------------------------------------");
+        System.out.println("\tDefault Format: = "+ fnum);
+        System.out.println("\t Type 1: = " + String.format("%4.2f", fnum));
+        System.out.println("\t Type 2: = " + String.format("%.5f", fnum));
+
+        System.out.println();
+        input.close();
+    }
+}
